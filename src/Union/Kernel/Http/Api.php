857260920a37f89f201cd4bafe3c5c01235c2882
bryan <wangyabener@gmail.com>
2019-10-25 14:09:40 +0800

    调用京东接口错误信息errorResonse改为error_response

--- src/Union/Kernel/Http/Api.php
+++ src/Union/Kernel/Http/Api.php
@@ -86,7 +86,7 @@ class Api extends UnionApiIterator
         $response = call_user_func_array([$this->param['http'],'post'],[self::URL,$data]);
         $this->status = $response->status;
         $content = $response->content;
-        if(isset($content['errorResponse'])){
+        if(isset($content['error_response'])){
             throw new UnionException(json_encode($content));
         }
         array_walk_recursive ($content,function($value,$key)use($requestParams){
